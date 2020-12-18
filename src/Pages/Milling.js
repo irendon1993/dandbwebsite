47c94ec082803650f717b3aebe795a0f35d25b1f
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2020-12-18 08:17:35 -0500

    Started working on milling page

--- src/Pages/Milling.js
+++ src/Pages/Milling.js
@@ -1,10 +1,86 @@
 import React from 'react'
+import MillingItem from '../Images/ImageCNCMenuItem.jpg'
 import { Link } from 'react-router-dom'
 
 export function Milling() {
     return(
-        <div>
-            test
+        <div className="milling">
+            <section className="millingHero">
+                <img src={MillingItem}></img>
+            </section>
+            <section className="millingEquipment">
+                    <div className="millingIntro">
+                        <h2>
+                            MILLING CENTERS
+                        </h2>
+                        <p>
+                            3 and 4-axis horizontal and vertical machining centers
+                            with milling bed length up to 40 inches
+                        </p>
+                           
+                    </div>
+                    <div className="millingBody">
+                        <div className="leftText">
+                        <span>Matsuura</span>
+                            <p>
+                                1 - Matsuura HPlus 405 Horizontal Machining CTR
+                                12 Pallet Pool. 240 Tool ATC
+                                1,000 psi high pressure coolant and tool management
+                                for lights out operation
+                            </p>
+                            <p>
+                                4 - Mori-Seiki NH4000 Horizontal Machining Centers
+                                3 Pallet APC – 60 tool ATC
+                                30 h.p. 14,000 rpm spindle
+                                1,000 psi high pressure coolant system
+                            </p>
+                        
+                            <p>
+                                1 - Mori-Seiki DuraVertical 5100 Machining Center
+                                25 h.p. 12,000 rpm spindle with 30 tool ATC
+                                20” x 40" table
+                            </p>
+                            <p>
+                                2 - Mori-Seiki DuraVertical 5060 Machining Centers
+                                10,000 rpm spindle with 30 tool ATC
+                                Nikken 4th axis
+                                1,000 psi high pressure coolant system
+                            </p>
+                        </div>
+                        <div className="rightText">
+                            <p>           
+                                1 - Brother Speedio R450X1 Machining Center
+                                2 Pallet APC with 22 tool ATC
+                                16,000 RPM Spindle
+                                High PSI
+                            </p>
+                            <p>
+                                1 - Brother Speedio R650X1 Machining Center
+                                2 Pallet APC with 22 tool ATC
+                                16,000 RPM Spindle
+                                High PSI 
+                            </p>
+                            <p>
+                                1 - Doosan VC510 Vertical Machining Center
+                                25 h.p. 10,000 rpm spindle with 30 tool ATC
+                                2 Pallet APC 22” x 33"
+                            </p>
+                            <p>
+                                1 - Doosan VC430 Vertical Machining Center
+                                25 h.p. 12,000 rpm spindle with 40 tool ATC
+                                4th Axis x 2 Pallet APC 19” x 28"
+                            </p>
+                            <p>
+                                1 - Doosan VC360 Vertical Machining Center
+                                25 h.p. 12,000 rpm spindle with 20 tool ATC
+                                2 Pallet APC 15” x 25"
+                            </p>
+                            <p>
+                                1 - Miltronics MB20 CNC Tool Room Mill
+                            </p>
+                        </div>
+                    </div>
+            </section>
         </div>
     );
 }
