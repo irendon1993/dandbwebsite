e90a990031e572036f4752f8cf7d806ea93c443e
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2021-01-13 12:36:25 -0500

    finsihed Home api calls

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
 import HeroVideo from '../Videos/VideoHomeHero.mp4'
 import Hero from '../Images/ImageHalter.jpg' 
@@ -7,9 +7,27 @@ import Kanban from '../Images/ImageKanban.jpg'
 import Quality from '../Images/ImageQuality.jpg'
 
 
-
-
 export function Home() {
+
+    
+    const [homeData, setHomeData] = useState([])
+
+  async function fetchTest() {
+    const response = await fetch(
+        'https://strapi-mongo-backend.herokuapp.com/home-pages'
+    )
+  
+    const json = await response.json()
+  
+    setHomeData(json)
+  }
+
+  useEffect(()=> {
+    fetchTest()
+  },[])
+
+  console.log(homeData)
+
     return (
         <div className="home">
             <header class="v-header container">
@@ -20,7 +38,7 @@ export function Home() {
                 <div class="header-overlay"></div>
                 <div class="header-content text-md-center">
                     <h1>Quality Engineered Solutions</h1>
-                    <p>Technologically equipped with state of the art milling and turning centers to handle any unique machine part your business may require.</p>
+                    {homeData && homeData.map(homeData => (<p>{homeData.HeroText}</p>))}
                     <a class="btn">Find Out More</a>
                 </div>
             </header>
@@ -33,45 +51,21 @@ export function Home() {
                 
                 <div className="lightsText">
                     <h3>Lights Out Manufacturing</h3>
-                <p>Precision matching of components from many materials, typically stainless steel, carbon steel,
-                    aluminum alloys, plastics and other non-ferrous materials.
-                </p>
-
-                <p className="smallP">
-                    D & B Machine uses automated parts loaders, automatic tool changers and computer controls
-                    that allow machine tools to operate around the clock, at times totally unattended. Operating two
-                    shifts D & B's skilled machinists each monitor fromone to four machines and chart to accuracy parts
-                    being produced using SPC.
-                </p>
+                    {homeData && homeData.map(homeData => (<p>{homeData.LightsFirstParagraph}</p>))}
+                    {homeData && homeData.map(homeData => (<p>{homeData.LightsSecondParagraph}</p>))}
+            
                 <Link to="/solutions"><button className="push-button-3d"><span>Learn More</span></button></Link>
                 </div>
             </div>
             <div className="kanban">
                 <div className="kanbanText">
-                <h3>Kanban Scheduling</h3>
-                <p>
-                    D & B Machine manufactures and inventories 
-                    components for clients as needed for final 
-                    assembly. Components are produced in
-                    batches at D & B Machine to amortize client
-                    machining setup costs and sequenced
-                    by part to minimize the number of client
-                    tool changes.
-                </p>
-                <p>
-                    By monitoring client demand, 
-                    D & B Machine determines what products
-                    in what quantities need to be machined 
-                    and prepared for delivery. D & B Machine
-                    maintains secure, climate-controlled 
-                    warehousing space to inventory parts for
-                    our Kanban clients.
-                </p>
-                
-                <Link to="/solutions"><button className="push-button-3d"><span>Learn More</span></button></Link>
+                    <h3>Kanban Scheduling</h3>
+                    {homeData && homeData.map(homeData => (<p>{homeData.KanbanFirstParagraph}</p>))}
+                    {homeData && homeData.map(homeData => (<p>{homeData.KanbanSecondParagraph}</p>))}
+                    <Link to="/solutions"><button className="push-button-3d"><span>Learn More</span></button></Link>
                 </div>
                 <div className="kanbanImage">
-                <img src={Kanban}></img>
+                    <img src={Kanban}></img>
                 </div>
             </div> 
             <div className="qualityAssurance">
@@ -80,16 +74,8 @@ export function Home() {
                 </div>
                 <div className="qualityText">
                     <h3>Quality Assurance</h3>
-                    <p>D & B Machine is ISO 9001: 2015 Certified to
-                        manufacture (Mil.)components for military and
-                        civilian equipment.
-                    </p>
-                    <p>
-                        D & B Machine routinely upgrades its inspection 
-                        capabilities to improve the efficiency and accuracy
-                        of the quality assurance process. Our complete line
-                        of inspection equipment is calibrated and certified.
-                    </p>
+                    {homeData && homeData.map(homeData => (<p>{homeData.QualityFirstParagraph}</p>))}
+                    {homeData && homeData.map(homeData => (<p>{homeData.QualitySecondParagraph}</p>))}
                     <Link to="/quality"><button className="push-button-3d"><span>Learn More</span></button></Link>
                 </div>
                 
