34f28e3580a3983974438e454a5d9ce0c0d72471
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2021-02-05 08:09:57 -0500

    Finsihed laser page

--- src/Pages/Laser.jsx
+++ src/Pages/Laser.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, {useEffect,useState} from 'react'
+import LaserHero from "../Images/laserHero.jpg"
 import { Link } from 'react-router-dom'
 
 export function Laser() {
+    
+    const [testData, setLaserData] = useState([])
+
+  async function laserTest() {
+    const response = await fetch(
+        'https://strapi-mongo-backend.herokuapp.com/about-pages'
+    )
+  
+    const json = await response.json()
+  
+    setLaserData(json)
+  }
+
+  useEffect(()=> {
+    laserTest()
+  },[])
+
+  console.log(testData)
+
     return(
-        <div>
-            test
+
+        
+        <div className="laser">
+            <h1>Laser Engraving</h1>
+
+            <div className="laserBox">
+                <div className="laserHero">
+                    <img src={LaserHero} />
+                </div>
+                <div className="laserText">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias aliquid vero repudiandae aspernatur a impedit vitae rem natus itaque totam doloremque nostrum voluptate, ullam optio autem distinctio ab expedita consequatur.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum possimus quidem voluptatum officia dolores deserunt dolorum id cupiditate nisi provident consectetur sed doloremque quas, molestiae reiciendis architecto modi totam cum.
+                    </p>
+                
+                </div>
+            </div>
+            
+            
+            
         </div>
     );
 }
