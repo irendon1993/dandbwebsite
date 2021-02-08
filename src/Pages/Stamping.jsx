d1e0951621af2d0c3862ecce30a4da7b05fef8fb
Isaac Rendon <isaacrendon7@gmail.com>
2021-02-08 08:50:46 -0500

    Added metal stamping text

--- src/Pages/Stamping.jsx
+++ src/Pages/Stamping.jsx
@@ -1,31 +1,33 @@
-import React, {useEffect,useState} from 'react'
-import StampingHero from "../Images/stampingHero.jpg"
-import { Link } from 'react-router-dom'
-
+import React, { useEffect, useState } from "react";
+import StampingHero from "../Images/stampingHero.jpg";
+import { Link } from "react-router-dom";
 
 export function Stamping() {
-
-return(
-
-        
+  return (
     <div className="stamping">
-        <h1>Metal Stamping </h1>
+      <h1>Metal Stamping </h1>
 
-        <div className="stampingBox">
-            <div className="stampingHero">
-                <img src={StampingHero} />
-            </div>
-            <div className="stampingText">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias aliquid vero repudiandae aspernatur a impedit vitae rem natus itaque totam doloremque nostrum voluptate, ullam optio autem distinctio ab expedita consequatur.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum possimus quidem voluptatum officia dolores deserunt dolorum id cupiditate nisi provident consectetur sed doloremque quas, molestiae reiciendis architecto modi totam cum.
-                </p>
-            
-            </div>
+      <div className="stampingBox">
+        <div className="stampingHero">
+          <img src={StampingHero} />
         </div>
-        
-        
-        
+        <div className="stampingText">
+          <p>
+            Machines for stamping press processing thin metal plates with dies.
+            Used in processes for manufacturing electronic components such as
+            terminals, and products such as automotive parts that require
+            drawing processing.
+          </p>
+          <br />
+          <p>
+            Equipment List:
+            <ol>
+              <li>AMADA SDE-1120 Digital Electric Servo Press -121 TON</li>
+              <li>Komatsu 25, and 60 Ton Punch Presses</li>
+            </ol>
+          </p>
+        </div>
+      </div>
     </div>
-);
+  );
 }
